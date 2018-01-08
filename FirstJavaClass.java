90cd0c6a2a6176b712a89f5d6871ca445a85f1a8
rpeddinti1 <34720277+rpeddinti1@users.noreply.github.com>
2018-01-07 18:20:48 -0600

    FirstJavaClass
    First Java Class

---
+++ FirstJavaClass.java
@@ -0,0 +1,12 @@
+public class FirstJavaClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Java World");
+
+		System.out.println("welcome to Java");
+		
+		
+	}
+
+}
